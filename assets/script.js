cdaed7c3cc0bc2d99a3c33a968339d7ec6c66d84
Rucatues <115330764+Rucatues@users.noreply.github.com>
2023-01-18 07:57:04 -0600

    linked day.js and jquery

---
+++ assets/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+
+    console.log("Five Day Forecast")
+
+    // Sets current date for current weather block 
+    $('#currentDay').text(dayjs().format('MMMM D, YYYY'));
+
+
+}); 
